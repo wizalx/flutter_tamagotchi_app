d29c152bf47d9ff5dadeb85fc91fb54127efc5c9
WizAlx <wizalx@gmail.com>
2022-01-16 00:18:26 +0500

    use about it

--- lib/pages/tamagotchi_profile.dart
+++ lib/pages/tamagotchi_profile.dart
@@ -9,17 +9,110 @@ class TamaProfScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Wrap(
-          alignment: WrapAlignment.center,
-          spacing: 10, // to apply margin in the main axis of the wrap
-          runSpacing: 10,
-          children: [
-              StatusWidget(),
-              ProfileWidget(),
-              ProfControlBtnWidget()
-          ],
-        )
+      // body: Center(
+      //   child: Wrap(
+      //     alignment: WrapAlignment.center,
+      //     spacing: 10, // to apply margin in the main axis of the wrap
+      //     runSpacing: 10,
+      //     children: [
+      //         StatusWidget(),
+      //         ProfileWidget(),
+      //         ProfControlBtnWidget()
+      //     ],
+      //   )
+      // ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.red,
+          ),
+          Container(
+            height: 100,
+            color: Colors.blue,
+          ),
+          SizedBox(
+            height: 100,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                  child: Container(
+                    color: Colors.pink,
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    color: Colors.green,
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 100,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                  child: Container(
+                    color: Colors.yellow,
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    color: Colors.blue,
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 120,
+            child: Center(
+              child: Container(
+                width: 120,
+                color: Colors.pink,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 50,
+            child: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Container(
+                    width: 50,
+                    height: 50,
+                    color: Colors.pink,
+                  ),
+                  Container(
+                    width: 50,
+                    height: 50,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    width: 50,
+                    height: 50,
+                    color: Colors.green,
+                  ),
+                  Container(
+                    width: 50,
+                    height: 50,
+                    color: Colors.blue,
+                  )
+                ],
+              ),
+            )
+          )
+        ],
       ),
     );
   }
